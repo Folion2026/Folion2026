248d15ba34f89df6373db5574d9248d2e45db729
Pozzi <antonio.pozzi@hatch.com>
2026-07-02 19:23:34 +1000

    feat: add persistent Studio project sheets

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,10 @@ import NewProject from './pages/NewProjectEntry'
 import CollectionPage from './pages/Collection'
 import Studio from './pages/Studio'
 import StudioPackage from './pages/StudioPackage'
+import StudioNew from './pages/StudioNew'
 import CompanyProfile from './pages/CompanyProfile'
 import Magazine from './pages/Magazine'
 import FolionId from './pages/FolionId'
 import {AuthProvider} from './auth'
 import AuthGate from './components/AuthGate'
-export default function App(){return <AuthProvider><StoreProvider><BrowserRouter><Routes><Route path="/" element={<Landing/>}/><Route path="/sign-in" element={<AuthGate><Navigate to="/home" replace/></AuthGate>}/><Route path="/magazine" element={<Magazine/>}/><Route path="/company/:id" element={<CompanyProfile/>}/><Route element={<AuthGate><AppShell/></AuthGate>}><Route path="/home" element={<Home/>}/><Route path="/projects" element={<Projects/>}/><Route path="/projects/:id" element={<ProjectDetail/>}/><Route path="/new-project" element={<NewProject/>}/><Route path="/project-reflection" element={<Navigate to="/new-project" replace/>}/><Route path="/collections/:id" element={<CollectionPage/>}/><Route path="/studio" element={<Studio/>}/><Route path="/studio/:id" element={<StudioPackage/>}/><Route path="/folion-id" element={<FolionId/>}/></Route></Routes></BrowserRouter></StoreProvider></AuthProvider>}
+export default function App(){return <AuthProvider><StoreProvider><BrowserRouter><Routes><Route path="/" element={<Landing/>}/><Route path="/sign-in" element={<AuthGate><Navigate to="/home" replace/></AuthGate>}/><Route path="/magazine" element={<Magazine/>}/><Route path="/company/:id" element={<CompanyProfile/>}/><Route element={<AuthGate><AppShell/></AuthGate>}><Route path="/home" element={<Home/>}/><Route path="/projects" element={<Projects/>}/><Route path="/projects/:id" element={<ProjectDetail/>}/><Route path="/new-project" element={<NewProject/>}/><Route path="/project-reflection" element={<Navigate to="/new-project" replace/>}/><Route path="/collections/:id" element={<CollectionPage/>}/><Route path="/studio" element={<Studio/>}/><Route path="/studio/new/:type" element={<StudioNew/>}/><Route path="/studio/:id" element={<StudioPackage/>}/><Route path="/folion-id" element={<FolionId/>}/></Route></Routes></BrowserRouter></StoreProvider></AuthProvider>}
